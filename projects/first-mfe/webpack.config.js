08f1f329d3b25c0059f4b7a01579949c8f8acc03
Victor Magalhães <victormagalhaessales@gmail.com>
2023-02-01 11:12:25 -0300

    Creating two simple microfrontends

---
+++ projects/first-mfe/webpack.config.js
@@ -0,0 +1,20 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+const isProduction = process.env.NODE_ENV == 'production';
+const stylesHandler = 'style-loader';
+
+const config = {
+    entry: './src/index.js',
+    output: {
+        path: path.resolve(__dirname, '../../dist/first-mfe')
+    },
+    plugins: [
+        new HtmlWebpackPlugin( { template: 'src/index.html' } )
+    ]
+};
+
+module.exports = () => {
+    config.mode = isProduction ? 'production' : 'development';
+    return config;
+};
